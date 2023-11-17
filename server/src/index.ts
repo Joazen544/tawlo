bc0439ef6cddeea7cceb649553b903f67511f77b
Joazen <johnsons544@gmail.com>
2023-11-17 15:51:30 +0800

    add 'express.json()' and 'process.env.PORT' to src/index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,16 @@
 import './dotenv';
 import express from 'express';
+import userRouter from './routes/user';
 
 const app = express();
+app.use(express.json());
+
+app.use('api', [userRouter]);
 
 app.get('/', (req, res) => {
   res.send(req.body);
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('port running on 3000');
 });
