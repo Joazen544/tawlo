c0fa3880f7b395a43fc65c20e53d40d76045316c
Joazen <johnsons544@gmail.com>
2023-11-17 20:31:11 +0800

    feat: add bcrypt to signup password

--- server/src/models/user.ts
+++ server/src/models/user.ts
@@ -1,16 +1,53 @@
 import mongoose from 'mongoose';
 import { ObjectId } from 'mongodb';
+import validator from 'validator';
+import bcrypt from 'bcrypt';
 
-const userSchema = new mongoose.Schema({
+interface UserDocument extends Document {
+  name: string;
+  email: string;
+  password: string;
+  password_confirm: string;
+  read_posts: ObjectId[];
+  friends: ObjectId[];
+  follow: ObjectId[];
+  block: ObjectId[];
+  read_board: ObjectId[];
+  preference_tags: { id: ObjectId; number: Number }[];
+  chats: ObjectId[];
+  upvote: number;
+  downvote: number;
+  honor_now: { name: string; id: ObjectId };
+  honors: { name: string; id: ObjectId }[];
+}
+
+const userSchema = new mongoose.Schema<UserDocument>({
   name: {
     type: String,
-    required: [true, 'A user must have a name'],
+    required: [true, 'Please tell us your name'],
   },
   email: {
     type: String,
     unique: true,
     lowercase: true,
-    required: true,
+    required: [true, 'Please offer your email'],
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: [8, 'Password must contain at least 8 characters'],
+  },
+  password_confirm: {
+    type: String || undefined,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      // This only works on SAVE!!!
+      validator(this: UserDocument, el: String) {
+        return el === this.password;
+      },
+      message: 'Password are not the same',
+    },
   },
   // Posts read 300 recorded
   read_posts: [ObjectId],
@@ -44,4 +81,15 @@ const userSchema = new mongoose.Schema({
   ],
 });
 
-export default mongoose.model('User', userSchema);
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password and delete the confirm
+  this.password = await bcrypt.hash(this.password, 12);
+
+  this.password_confirm = 'confirm';
+  return next();
+});
+
+export default mongoose.model<UserDocument>('User', userSchema);
