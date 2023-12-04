b5545a0b69f1df8e3c60c731ff356f95232bbcb5
Joazen <johnsons544@gmail.com>
2023-12-04 18:17:53 +0800

    feat: complete the meeting feature
    now user can create a meeting, accept or refuse a match, and once both user accept the match,
    a message group will be established.
    After user give the meeting a score, user can start a new meeting

--- tawlo-react/src/Meeting.tsx
+++ tawlo-react/src/Meeting.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Header from './components/HeaderElements/Header';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
 import axios from 'axios';
@@ -14,6 +14,7 @@ const Meeting = () => {
   const token = Cookies.get('jwtToken');
 
   const [meetingStatus, setMeetingStatus] = useState('');
+  const [meetingId, setMeetingId] = useState('');
   const [jobTitle, setJobTitle] = useState('');
   const [toAskInputText, setToAskInputText] = useState<string>('');
   const [toAskItems, setToAskItems] = useState<Item[]>([]);
@@ -25,6 +26,16 @@ const Meeting = () => {
   const [userIntro, setUserIntro] = useState<string>('');
   const [userToShare, setUserToShare] = useState<string[]>([]);
   const [userToAsk, setUserToAsk] = useState<string[]>([]);
+  const [targetRole, setTargetRole] = useState<string>('');
+  const [targetIntro, setTargetIntro] = useState<string>('');
+  const [targetToShare, setTargetToShare] = useState<string[]>([]);
+  const [targetToAsk, setTargetToAsk] = useState<string[]>([]);
+  const [targetRating, setTargetRating] = useState<number>(0);
+  const [targetId, setTargetId] = useState<string>('');
+  const [targetName, setTargetName] = useState<string>('');
+  const [score, setScore] = useState<number>();
+  const [comment, setComment] = useState<string>('');
+  const [commentError, setCommentError] = useState<string>('');
 
   useEffect(() => {
     axios
@@ -55,8 +66,62 @@ const Meeting = () => {
           setUserToShare(res.data.meeting.user.to_share);
           setUserToAsk(res.data.meeting.user.to_ask);
         });
+    } else if (meetingStatus === 'checking' || meetingStatus === 'waiting') {
+      axios
+        .get(`http://localhost:3000/api/meeting`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setUserRole(res.data.meeting.user.role);
+          setUserIntro(res.data.meeting.user.user_intro);
+          setUserToShare(res.data.meeting.user.to_share);
+          setUserToAsk(res.data.meeting.user.to_ask);
+          setTargetRole(res.data.meeting.target.role);
+          setTargetIntro(res.data.meeting.target.user_intro);
+          setTargetRating(res.data.meeting.target.rating);
+          setTargetToShare(res.data.meeting.target.to_share);
+          setTargetToAsk(res.data.meeting.target.to_ask);
+          setMeetingId(res.data.meeting._id);
+        });
+    } else if (meetingStatus === 'end') {
+      axios
+        .get(`http://localhost:3000/api/meeting`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        })
+        .then((res) => {
+          console.log(res.data);
+          setUserRole(res.data.meeting.user.role);
+          setUserIntro(res.data.meeting.user.user_intro);
+          setUserToShare(res.data.meeting.user.to_share);
+          setUserToAsk(res.data.meeting.user.to_ask);
+          setTargetRole(res.data.meeting.target.role);
+          setTargetIntro(res.data.meeting.target.user_intro);
+          setTargetRating(res.data.meeting.target.rating);
+          setTargetToShare(res.data.meeting.target.to_share);
+          setTargetToAsk(res.data.meeting.target.to_ask);
+          setTargetId(res.data.meeting.target.userId);
+          setMeetingId(res.data.meeting._id);
+        });
     }
   }, [meetingStatus]);
+
+  useEffect(() => {
+    if (targetId) {
+      axios
+        .get(`http://localhost:3000/api/user/name?id=${targetId}`)
+        .then((res) => {
+          setTargetName(res.data.name);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  }, [targetId]);
 
   const handleJobTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setJobTitle(e.target.value);
@@ -172,12 +237,103 @@ const Meeting = () => {
       });
   };
 
+  const handleRefuseMeeting = () => {
+    axios.post(
+      `http://localhost:3000/api/meeting/${meetingId}`,
+      {
+        reply: 'deny',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          authorization: `Bearer ${token}`,
+        },
+      },
+    );
+  };
+
+  const handleAcceptMeeting = () => {
+    axios
+      .post(
+        `http://localhost:3000/api/meeting/${meetingId}`,
+        {
+          reply: 'accept',
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`,
+          },
+        },
+      )
+      .then((res) => {
+        if (res.data.situation && res.data.situation === 'first') {
+          setMeetingStatus('waiting');
+          return;
+        } else if (res.data.situation && res.data.situation === 'second') {
+          setMeetingStatus('end');
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  const handleScoreChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setScore(+e.target.value);
+  };
+
+  const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setComment(e.target.value);
+  };
+
+  const handleCommentSubmit = () => {
+    if (!score) {
+      setCommentError('記得給對方一個分數！');
+      return;
+    }
+    if (score > 5 || score < 0) {
+      setCommentError('分數要介於 0~5！');
+      return;
+    }
+    if (!comment) {
+      setCommentError('記得給對方一些評價，讓其他人可以參考！');
+      return;
+    }
+    if (!targetId) {
+      console.log('something is wrong, target id is missing');
+      return;
+    }
+
+    axios
+      .post(
+        `http://localhost:3000/api/meeting/${meetingId}/score`,
+        {
+          targetUser: targetId,
+          comment: comment,
+          score: score,
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            authorization: `Bearer ${token}`,
+          },
+        },
+      )
+      .then(() => {
+        setMeetingStatus('none');
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
   return (
     <>
       {!token && <Navigate to={'/user/signin'} replace={true}></Navigate>}
       <Header />
       <div className="w-full flex justify-center">
-        <div className="bg-gray-200 w-1/2 mt-52">
+        <div className="bg-gray-200 w-1/2 mt-20">
           {meetingStatus === 'none' && (
             <div className="flex flex-col items-center pl-10 pr-10 pt-10 w-full h-full">
               <h1 className="text-2xl">請填寫配對資訊</h1>
@@ -295,7 +451,7 @@ const Meeting = () => {
                     {userToAsk.map((item, index) => (
                       <div
                         key={item + index}
-                        className="border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
                       >
                         <span>{item}</span>
                       </div>
@@ -308,7 +464,7 @@ const Meeting = () => {
                     {userToShare.map((item, index) => (
                       <div
                         key={item + index}
-                        className="border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
                       >
                         <span>{item}</span>
                       </div>
@@ -327,6 +483,234 @@ const Meeting = () => {
                 >
                   取消
                 </button>
+              </div>
+            </div>
+          )}
+          {meetingStatus === 'checking' && (
+            <div className="flex flex-col items-center pl-10 pr-10 pt-10 w-full h-full">
+              <h1 className="text-2xl">這裡是你的配對對象 （對對...）</h1>
+              <div id="userInfoContainer" className="w-full h-full  m-10">
+                <div id="role">
+                  <div>他的職稱</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-8 flex items-center">
+                    {targetRole}
+                  </div>
+                </div>
+                <div id="score" className="mt-5">
+                  <div>他的評分</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-8 flex items-center">
+                    {targetRating}
+                  </div>
+                </div>
+                <div id="toAsk" className="mt-5">
+                  <div>他想了解的領域</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-10 flex items-center">
+                    {targetToAsk.map((item, index) => (
+                      <div
+                        key={item + index}
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                      >
+                        <span>{item}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div id="toShare" className="mt-5">
+                  <div>他想分享的領域</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-10 flex items-center">
+                    {targetToShare.map((item, index) => (
+                      <div
+                        key={item + index}
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                      >
+                        <span>{item}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div id="userIntro" className="mt-5">
+                  <div>他的自我介紹</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 min-h-10 flex items-center">
+                    <p>{targetIntro}</p>
+                  </div>
+                </div>
+
+                <button
+                  onClick={handleAcceptMeeting}
+                  className="mr-3 mt-6 px-3 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600"
+                >
+                  接受
+                </button>
+                <button
+                  onClick={handleRefuseMeeting}
+                  className="mt-6 px-3 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                  拒絕
+                </button>
+              </div>
+            </div>
+          )}
+          {meetingStatus === 'waiting' && (
+            <div className="flex flex-col items-center pl-10 pr-10 pt-10 w-full h-full">
+              <h1 className="text-2xl">已同意配對，等對方一下子</h1>
+              <div id="userInfoContainer" className="w-full h-full  m-10">
+                <div id="role">
+                  <div>他的職稱</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-8 flex items-center">
+                    {targetRole}
+                  </div>
+                </div>
+                <div id="score" className="mt-5">
+                  <div>他的評分</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-8 flex items-center">
+                    {targetRating}
+                  </div>
+                </div>
+                <div id="toAsk" className="mt-5">
+                  <div>他想了解的領域</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-10 flex items-center">
+                    {targetToAsk.map((item, index) => (
+                      <div
+                        key={item + index}
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                      >
+                        <span>{item}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div id="toShare" className="mt-5">
+                  <div>他想分享的領域</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-10 flex items-center">
+                    {targetToShare.map((item, index) => (
+                      <div
+                        key={item + index}
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                      >
+                        <span>{item}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div id="userIntro" className="mt-5">
+                  <div>他的自我介紹</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 min-h-10 flex items-center">
+                    <p>{targetIntro}</p>
+                  </div>
+                </div>
+
+                <button
+                  disabled
+                  className="mr-3 mt-6 px-3 py-2 bg-blue-300 text-white rounded-md cursor-not-allowed"
+                >
+                  已接受
+                </button>
+                <button
+                  onClick={handleRefuseMeeting}
+                  className="mt-6 px-3 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+                >
+                  拒絕
+                </button>
+              </div>
+            </div>
+          )}
+          {meetingStatus === 'end' && (
+            <div className="flex flex-col items-center pl-10 pr-10 pt-10 w-full h-full">
+              <h1 className="text-2xl">
+                成功配對！與 {targetName} 的聊天室已建立
+              </h1>
+              <div id="userInfoContainer" className="w-full h-full  m-10">
+                <div id="name" className="mb-2">
+                  <div className="mb-2">對方的名字</div>
+
+                  <Link
+                    to={`/user/profile/${targetId}`}
+                    className="w-20 text-left text-blue-400 text-lg"
+                  >
+                    {targetName}
+                  </Link>
+                </div>
+                <div id="role">
+                  <div>他的職稱</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-8 flex items-center">
+                    {targetRole}
+                  </div>
+                </div>
+                <div id="score" className="mt-5">
+                  <div>他的評分</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-8 flex items-center">
+                    {targetRating}
+                  </div>
+                </div>
+                <div id="toAsk" className="mt-5">
+                  <div>他想了解的領域</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-10 flex items-center">
+                    {targetToAsk.map((item, index) => (
+                      <div
+                        key={item + index}
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                      >
+                        <span>{item}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div id="toShare" className="mt-5">
+                  <div>他想分享的領域</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 h-10 flex items-center">
+                    {targetToShare.map((item, index) => (
+                      <div
+                        key={item + index}
+                        className="ml-2 border-solid border-2 border-blue-300 rounded-md bg-blue-400 text-white p-1 h-8"
+                      >
+                        <span>{item}</span>
+                      </div>
+                    ))}
+                  </div>
+                </div>
+                <div id="userIntro" className="mt-5">
+                  <div>他的自我介紹</div>
+                  <div className="bg-gray-300 p-2 rounded-md mt-2 min-h-10 flex items-center">
+                    <p>{targetIntro}</p>
+                  </div>
+                </div>
+
+                <div className="mt-10">
+                  <div id="score" className="mb-2">
+                    <div className="mb-2">給這位分享者 0~5 分的評價</div>
+                    <input
+                      type="number"
+                      value={score}
+                      onChange={handleScoreChange}
+                      className="pl-2 w-20"
+                      max={5}
+                      min={0}
+                      placeholder="0~5"
+                    />
+                  </div>
+                  <div id="comment">
+                    <div className="mb-2">
+                      這位分享者如何？給其他配對者一個參考
+                    </div>
+                    <textarea
+                      value={comment}
+                      onChange={handleCommentChange}
+                      className="pl-2 w-full h-32"
+                    />
+                  </div>
+                  <div className="mt-5">
+                    注意：送出評論後，這個頁面會消失。並且可以重新配對新的對象
+                  </div>
+                  <button
+                    onClick={handleCommentSubmit}
+                    className="mr-3 mt-6 px-3 py-2 bg-blue-500 hover:bg-blue-600 text-white rounded-md"
+                  >
+                    送出評論
+                  </button>
+                  {commentError && (
+                    <p className="text-red-500">{commentError}</p>
+                  )}
+                </div>
               </div>
             </div>
           )}
